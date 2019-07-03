0306455c2c0c516d496025b2faf5c9833b3f956d
brajcic <bojanrajcic25@gmail.com>
2019-07-03 08:14:51 +0200

    Created model, basic check if email&pw exist in database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
+
+use App\Users;
 
 class AuthController extends Controller
 {
@@ -13,11 +19,28 @@ class AuthController extends Controller
 		$email = $request->email;
 		$password = $request->password;
 		
-		if(Auth::attempt(['email' => $email, 'password' => $password])){
+	
+		$allUsers = Users::all();
+	
+		$ind = 0;
+	
+		foreach($allUsers as $user){
+				
+				if(Hash::check($password,$user['password']))
+					$ind = 1;
 				
 			}
-			
+	
+		if ($ind != 0)
+			return "Moze se logovati";
+		else return "Ne moze se logovati";
+	
+		//$user = DB::table('users')->where('email', $email)->first();
 		
+		//$users = DB::table('users')->get();
+	 
+		
+		//$this->assertDatabaseHas('users',['email' => $email]);
 		
 		
 		}
